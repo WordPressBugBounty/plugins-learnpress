f324651f47f907e41333a4916948e0639d708605
Sean Murphy <sean@iamseanmurphy.com>
2024-11-12 04:30:37 +0000

    4.2.7.3

--- learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/Dynamic/CourseOfflineAddressDynamicElementor.php
+++ learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/Dynamic/CourseOfflineAddressDynamicElementor.php
@@ -5,29 +5,30 @@
  * Dynamic course level elementor.
  *
  * @since 4.2.3.5
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 namespace LearnPress\ExternalPlugin\Elementor\Widgets\Course\Dynamic;
 use Elementor\Core\DynamicTags\Tag;
 use LearnPress\ExternalPlugin\Elementor\LPDynamicElementor;
 use LearnPress\Models\CourseModel;
+use LearnPress\TemplateHooks\Course\SingleCourseOfflineTemplate;
 use LearnPress\TemplateHooks\Course\SingleCourseTemplate;
 use Throwable;
 
 defined( 'ABSPATH' ) || exit;
 
-class CourseCapacityDynamicElementor extends Tag {
+class CourseOfflineAddressDynamicElementor extends Tag {
 	use LPDynamicElementor;
 
 	public function __construct( array $data = [] ) {
-		$this->lp_dynamic_title = 'Course Capacity';
-		$this->lp_dynamic_name  = 'course-capacity';
+		$this->lp_dynamic_title = 'Course Address';
+		$this->lp_dynamic_name  = 'course-address';
 		parent::__construct( $data );
 	}
 
 	public function render() {
-		$singleCourseTemplate = SingleCourseTemplate::instance();
+		$singleCourseOfflineTemplate = SingleCourseOfflineTemplate::instance();
 
 		try {
 			$course = $this->get_course_model();
@@ -35,7 +36,12 @@ class CourseCapacityDynamicElementor extends Tag {
 				return;
 			}
 
-			echo $singleCourseTemplate->html_capacity( $course );
+			$course = CourseModel::find( $course->get_id(), true );
+			if ( ! $course ) {
+				return;
+			}
+
+			echo $singleCourseOfflineTemplate->html_address( $course );
 		} catch ( Throwable $e ) {
 			error_log( $e->getMessage() );
 		}
--- learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/Dynamic/CourseOfflineDeliverTypeDynamicElementor.php
+++ learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/Dynamic/CourseOfflineDeliverTypeDynamicElementor.php
@@ -5,29 +5,30 @@
  * Dynamic course level elementor.
  *
  * @since 4.2.3.5
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 namespace LearnPress\ExternalPlugin\Elementor\Widgets\Course\Dynamic;
 use Elementor\Core\DynamicTags\Tag;
 use LearnPress\ExternalPlugin\Elementor\LPDynamicElementor;
 use LearnPress\Models\CourseModel;
+use LearnPress\TemplateHooks\Course\SingleCourseOfflineTemplate;
 use LearnPress\TemplateHooks\Course\SingleCourseTemplate;
 use Throwable;
 
 defined( 'ABSPATH' ) || exit;
 
-class CourseCapacityDynamicElementor extends Tag {
+class CourseOfflineDeliverTypeDynamicElementor extends Tag {
 	use LPDynamicElementor;
 
 	public function __construct( array $data = [] ) {
-		$this->lp_dynamic_title = 'Course Capacity';
-		$this->lp_dynamic_name  = 'course-capacity';
+		$this->lp_dynamic_title = 'Course Deliver Type';
+		$this->lp_dynamic_name  = 'course-deliver-type';
 		parent::__construct( $data );
 	}
 
 	public function render() {
-		$singleCourseTemplate = SingleCourseTemplate::instance();
+		$singleCourseOfflineTemplate = SingleCourseOfflineTemplate::instance();
 
 		try {
 			$course = $this->get_course_model();
@@ -35,7 +36,7 @@ class CourseCapacityDynamicElementor extends Tag {
 				return;
 			}
 
-			echo $singleCourseTemplate->html_capacity( $course );
+			echo $singleCourseOfflineTemplate->html_deliver_type( $course );
 		} catch ( Throwable $e ) {
 			error_log( $e->getMessage() );
 		}
