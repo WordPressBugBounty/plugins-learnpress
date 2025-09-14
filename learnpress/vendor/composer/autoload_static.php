6b0dfac04a8b897cde09f88b9d80735de8feac5e
Sean Murphy <sean@iamseanmurphy.com>
2025-09-14 18:17:38 +0000

    4.2.9.2

--- learnpress/vendor/composer/autoload_static.php
+++ learnpress/vendor/composer/autoload_static.php
@@ -4,12 +4,8 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit8544e55cb892a0745c66576872b5abce
+class ComposerStaticInitc3b74577235e45899f610b3dca97944f
 {
-    public static $files = array (
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
         'T' => 
         array (
@@ -17,7 +13,6 @@ class ComposerStaticInit8544e55cb892a0745c66576872b5abce
         ),
         'S' => 
         array (
-            'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Component\\CssSelector\\' => 30,
         ),
         'L' => 
@@ -31,10 +26,6 @@ class ComposerStaticInit8544e55cb892a0745c66576872b5abce
         array (
             0 => __DIR__ . '/..' . '/tijsverkoyen/css-to-inline-styles/src',
         ),
-        'Symfony\\Polyfill\\Php80\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
-        ),
         'Symfony\\Component\\CssSelector\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/css-selector',
@@ -46,20 +37,15 @@ class ComposerStaticInit8544e55cb892a0745c66576872b5abce
     );
 
     public static $classMap = array (
-        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
-        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
-        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
-        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit8544e55cb892a0745c66576872b5abce::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit8544e55cb892a0745c66576872b5abce::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit8544e55cb892a0745c66576872b5abce::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc3b74577235e45899f610b3dca97944f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc3b74577235e45899f610b3dca97944f::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitc3b74577235e45899f610b3dca97944f::$classMap;
 
         }, null, ClassLoader::class);
     }
